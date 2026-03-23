0b2b06be38fad72e0e1c3b54b9f1d54b6485f2d3
ayal <bdu1404571@bdu.edu.et>
2026-03-24 00:07:53 +0300

    Add solution for Longest Turbulent Subarray (trial 1)

---
+++ leetcode/longest-turbulent-subarray_trial1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxTurbulenceSize(self, arr: List[int]) -> int:
+        n = len(arr)
+        if n < 2:
+            return n
+        
+        ans = 1
+        left = 0
+        
+        for right in range(1, n):
+            cmp = (arr[right] > arr[right - 1]) - (arr[right] < arr[right - 1])
+            
+            if cmp == 0:
+                left = right
+            elif right == n - 1 or cmp * ((arr[right + 1] > arr[right]) - (arr[right + 1] < arr[right])) != -1:
+                ans = max(ans, right - left + 1)
+                left = right
+        
+        return ans
