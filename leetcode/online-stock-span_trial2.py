2631914f665e807b0d35a338ce01334dccb1e5df
ayal <bdu1404571@bdu.edu.et>
2026-04-13 23:32:21 +0300

    Add solution for Online Stock Span (trial 2)

---
+++ leetcode/online-stock-span_trial2.py
@@ -0,0 +1,16 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price: int) -> int:
+        current_span = 1
+        while self.stack and self.stack[-1][0] <= price:
+            prev_price, prev_span = self.stack.pop()
+            current_span += prev_span
+            
+        self.stack.append((price, current_span))
+        return current_span
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
